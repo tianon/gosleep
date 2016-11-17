a625d2ae4ac2803a6035128f4de12f6da17b5aec
Tianon Gravi <admwiggin@gmail.com>
2016-11-16 16:22:34 -0800

    Add proper --for/--until support

--- src/gosleep/main.go
+++ src/gosleep/main.go
@@ -1,22 +1,75 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"time"
 
+	"github.com/jessevdk/go-flags"
 	"gopkg.in/cheggaaa/pb.v1"
+	jinzhuNow "github.com/jinzhu/now"
 )
+
+type sleepFlags struct {
+	For   bool `long:"for"`
+	Until bool `long:"until"`
+}
 
 func main() {
 	start := time.Now()
 
-	// TODO accept either duration or absolute time for sleep target
-	d, err := time.ParseDuration(os.Args[1])
+	opts := sleepFlags{}
+	flagParser := flags.NewParser(&opts, flags.Default)
+	flagParser.Usage = "--for <duration> OR --until <time>"
+
+	args, err := flagParser.Parse()
 	if err != nil {
-		panic(err)
+		if flagsErr, ok := err.(*flags.Error); ok {
+			if flagsErr.Type == flags.ErrHelp {
+				return
+			}
+		}
+		os.Exit(1)
+	}
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "error: argument expected\n\n")
+		flagParser.WriteHelp(os.Stderr)
+		os.Exit(1)
 	}
 
-	until := start.Add(d)
+	if opts.For == opts.Until {
+		fmt.Fprintf(os.Stderr, "error: exactly one of --for or --until expected\n\n")
+		flagParser.WriteHelp(os.Stderr)
+		os.Exit(1)
+	}
+
+	var until time.Time
+	if opts.For {
+		d, err := time.ParseDuration(args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: failed to parse duration %q: %v\n", args[0], err)
+			os.Exit(1)
+		}
+		until = start.Add(d)
+	} else { // opts.Until
+		until, err = jinzhuNow.New(start).Parse(args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: failed to parse time %q: %v\n", args[0], err)
+			os.Exit(1)
+		}
+	}
+
+	// TODO consider making these optional flags
+	increment := 500 * time.Millisecond
+	round := time.Second
+
+	start = start.Round(round)
+	until = until.Round(round)
+
+	if until.Before(start) {
+		fmt.Fprintf(os.Stderr, "error: requested sleep time in the past: %s\n", until.Sub(start))
+		os.Exit(1)
+	}
 
 	bar := pb.New64(until.Sub(start).Nanoseconds())
 	bar.ShowCounters = true
@@ -27,8 +80,8 @@ func main() {
 
 	bar.Start()
 	for now := time.Now(); now.Before(until); now = time.Now() {
-		bar.Set64(now.Round(time.Second).Sub(start.Round(time.Second)).Nanoseconds())
-		time.Sleep(500 * time.Millisecond)
+		bar.Set64(now.Round(round).Sub(start).Nanoseconds())
+		time.Sleep(increment)
 	}
 	bar.Finish()
 }
